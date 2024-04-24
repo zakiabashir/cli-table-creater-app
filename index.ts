871520e1d961f8813afb61133462752d24f86c2e
zakiabashir <nshafeys0@gmail.com>
2024-04-24 12:50:56 -0700

    push my code

---
+++ index.ts
@@ -0,0 +1,21 @@
+#!  /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import Table  from 'cli-table';
+
+
+const table = new Table({
+  head: ['Name', 'Age', 'Gender', 'Country' , "Roll Number",' subject'],
+  colWidths: [12, 12, 12 , 12, 12, 12]
+});
+
+table.push(
+  [ 'zakia',  "30", 'female','Pakistan', '00141704' ,'python'], 
+   ['alia',  "25",  'Female', 'USA', '0012345', 'typescript'] ,
+   ['ayesha',  "40",  'female', 'Canada', '189745', 'css'] 
+);
+
+console.log(table.toString());
+
+
+
